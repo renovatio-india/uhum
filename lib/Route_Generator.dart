092ddfcabb1f07608850a46937be9ecc0f7d756b
Vedant-Mhatre <vedantmh@gmail.com>
2020-09-19 02:37:00 +0530

    added wrapper package

--- lib/Route_Generator.dart
+++ lib/Route_Generator.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:uhum/screens/home/home.dart';
+import 'package:uhum/screens/wrapper.dart';
 
 import 'main.dart';
 
