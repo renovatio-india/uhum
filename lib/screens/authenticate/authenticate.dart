76b618d4713d070da6c3f357873c18ddb46b3fa8
Vedant-Mhatre <vedantmh@gmail.com>
2020-09-19 02:25:09 +0530

    minor changes

--- lib/screens/authenticate/authenticate.dart
+++ lib/screens/authenticate/authenticate.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 
-class Authenticate extends StatelessWidget {
+class Authenticate extends StatefulWidget {
+  @override
+  _AuthenticateState createState() => _AuthenticateState();
+}
+
+class _AuthenticateState extends State<Authenticate> {
   static const String ROUTNAME = "authScreen";
   @override
   Widget build(BuildContext context) {
