1718a456fc760206e11e79e62139942d9d8eb496
Vedant-Mhatre <vedantmh@gmail.com>
2020-09-19 03:14:18 +0530

    created wrapper

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:uhum/screens/home/home.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // return home page or authenticate page
+
+    return HomePage();
+  }
+}
