15590940b3b86b1fa317c2e4fb0b011b9ad0671f
Vedant-Mhatre <vedantmh@gmail.com>
2020-09-19 03:21:06 +0530

    added sign in page

---
+++ lib/screens/authenticate/sign_in.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class SignIn extends StatefulWidget {
+  @override
+  _SignInState createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Text("This is my signin page"),
+      ),
+    );
+  }
+}
