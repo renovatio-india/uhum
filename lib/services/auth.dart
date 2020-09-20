4992a03215dd28b8705715d1fbf8241119e5b44d
Vedant-Mhatre <vedantmh@gmail.com>
2020-09-21 02:39:59 +0530

    create tempUser obj and auth user stream

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,7 +1,18 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:uhum/models/user.dart';
 
 class Authservice {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // create tempUser obj based on Firebase User
+  tempUser _userFromFirebaseuser(User user) {
+    return user != null ? tempUser(uid: user.uid) : null;
+  }
+
+  // auth change user stream
+  Stream<User> get user {
+    return _auth.authStateChanges.map(_userFromFirebaseuser);
+  }
 
   //sign in using phone number
   Future signInPhone() async {
@@ -11,7 +22,7 @@ class Authservice {
       //due to some problem with _auth.signInWithPhoneNumber() used email &
       // password
       final User user = result.user;
-      return user;
+      return _userFromFirebaseuser(user);
     } catch (e) {
       print(e.toString());
       return null;
