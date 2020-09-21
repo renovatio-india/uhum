72141103e77a32b3da0dccb1a24614d0f4355547
Akshay Chopra <Akshay.Chopra@ibm.com>
2020-09-21 22:06:48 +0530

    Simple Registration page created

---
+++ lib/screens/registration/registration.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class Registration extends StatefulWidget {
+  @override
+  _RegistrationState createState() => _RegistrationState();
+}
+
+class _RegistrationState extends State<Registration> {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Spacer(),
+              Text(
+                'Verify your phone',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 22,
+                ),
+              ),
+              Spacer(),
+              Container(
+                height: size.height * 0.06,
+                width: size.width * 0.8,
+                decoration: BoxDecoration(
+                    color: Colors.orange[100],
+                    borderRadius: BorderRadius.circular(30),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          offset: Offset(0, 3),
+                          blurRadius: 7,
+                          spreadRadius: 3),
+                    ]),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 8.0, horizontal: 16.0),
+                  child: TextField(
+                    // textAlign: TextAlign.center,
+                    keyboardType: TextInputType.number,
+                    inputFormatters: <TextInputFormatter>[
+                      FilteringTextInputFormatter.digitsOnly
+                    ],
+                    decoration: InputDecoration(
+                        hintText: '10-digit Mobile Number',
+                        hintStyle: TextStyle(
+                          fontSize: 12,
+                          color: Colors.black,
+                        ),
+                        border: InputBorder.none),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+                child: Container(
+                  height: size.height * 0.06,
+                  width: size.width * 0.8,
+                  // alignment: Alignment.topLeft,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(30),
+                    child: FlatButton(
+                      color: Colors.blue,
+                      onPressed: () {},
+                      child: Text(
+                        'Continue',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Spacer(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
