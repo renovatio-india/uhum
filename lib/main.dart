93ba4dec8d10070d0b8d8dc4bbd45cb337894170
Vedant-Mhatre <vedantmh@gmail.com>
2020-09-25 23:14:02 +0530

    removed firestore packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'Route_Generator.dart';
 
 void main() {
