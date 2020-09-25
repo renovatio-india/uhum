0bf5b221bd3f53d7dc1130b8a23c48e1906f77a8
05vidhi <vidhibante113@gmail.com>
2020-09-25 13:19:02 +0530

    Registration Screen  Added

--- lib/screens/authenticate/registration.dart
+++ lib/screens/authenticate/registration.dart
@@ -1,88 +1,151 @@
 import 'package:flutter/material.dart';
 
-
 class Registration extends StatefulWidget {
+  final Function toggleView;
+  Registration({this.toggleView});
+
   @override
   _RegistrationState createState() => _RegistrationState();
 }
 
 class _RegistrationState extends State<Registration> {
-  final _formKey = GlobalKey<FormState>();
-
-  
-
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.only(top:300,left:10,right: 10),
-      child: Form(
-        key: _formKey,
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Form(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            SizedBox(height: 20.0),
+            SizedBox(height: 15.0),
             Container(
-              padding: EdgeInsets.only(left:20),
+              padding: EdgeInsets.only(left:15),
+              height: size.height * 0.06,
+                width: size.width * 0.8,
               decoration: BoxDecoration(
                 color: Colors.yellow[100],
                 borderRadius: BorderRadius.circular(100),
+                 boxShadow: [
+                      BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          offset: Offset(0, 3),
+                          blurRadius: 7,
+                          spreadRadius: 3),
+                    ],
               ),
-              
               child: TextFormField(
                 decoration: InputDecoration(
                     border: InputBorder.none, hintText: 'Full Name'),
-                    validator: (val) => val.isEmpty ? 'Enter full name' : null,
-                     onChanged: (val){
-                     
-                    },
+                validator: (val) => val.isEmpty ? 'Enter full name' : null,
+                onChanged: (val) {},
               ),
             ),
-            SizedBox(height: 20.0),
+            SizedBox(height: 15.0),
             Container(
-              padding: EdgeInsets.only(left:20),
+              padding: EdgeInsets.only(left:15),
+              height: size.height * 0.06,
+                width: size.width * 0.8,
               decoration: BoxDecoration(
                 color: Colors.deepPurple[100],
                 borderRadius: BorderRadius.circular(100),
+                 boxShadow: [
+                      BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          offset: Offset(0, 3),
+                          blurRadius: 7,
+                          spreadRadius: 3),
+                    ],
               ),
-              
               child: TextFormField(
                 decoration: InputDecoration(
                     border: InputBorder.none, hintText: 'Email Address'),
-                     validator: (val) => val.isEmpty ? 'Enter  Email' : null,
-                    onChanged: (val){
-                     
-                    },
+                validator: (val) => val.isEmpty ? 'Enter  Email' : null,
+                onChanged: (val) {},
               ),
             ),
-            SizedBox(height: 20.0),
+            SizedBox(height: 10.0),
             Container(
-              padding: EdgeInsets.only(left:20),
+              padding: EdgeInsets.only(left:15),
+              height: size.height * 0.06,
+                width: size.width * 0.8,
               decoration: BoxDecoration(
                 color: Colors.blue[100],
+                
                 borderRadius: BorderRadius.circular(100),
+                 boxShadow: [
+                      BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          offset: Offset(0, 3),
+                          blurRadius: 7,
+                          spreadRadius: 3),
+                    ],
               ),
-             
+              child: TextFormField(
+                decoration:
+                    InputDecoration(border: InputBorder.none, hintText: 'Phone Number'),
+                obscureText: true,
+                validator: (val) => val.isEmpty ? 'Enter Phone No.' : null,
+                onChanged: (val) {},
+              ),
+            ),
+            SizedBox(height: 15.0),
+             Container(
+              padding: EdgeInsets.only(left:15),
+              height: size.height * 0.06,
+                width: size.width * 0.8,
+              decoration: BoxDecoration(
+                color: Colors.orange[100],
+                borderRadius: BorderRadius.circular(100),
+                 boxShadow: [
+                      BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          offset: Offset(0, 3),
+                          blurRadius: 7,
+                          spreadRadius: 3),
+                    ],
+              ),
               child: TextFormField(
                 decoration:
                     InputDecoration(border: InputBorder.none, hintText: 'Age'),
-                    obscureText: true,
-                     validator: (val) => val.isEmpty ? 'Enter Age':null,
-                     onChanged: (val){
-                     
-                    },
+                obscureText: true,
+                validator: (val) => val.isEmpty ? 'Enter Age' : null,
+                onChanged: (val) {},
               ),
             ),
-            SizedBox(height: 20.0),
-            RaisedButton(
-                shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(18.0),
-                    ),
-              child: Text('Submit'),
-              color: Colors.lightBlueAccent,
-              onPressed: () async {
-               
-                }
-              
+            SizedBox(height: 15.0),
+            Container(
+               height: size.height * 0.06,
+                width: size.width * 0.8,
+              child: RaisedButton(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                  child: Text('Submit',style: TextStyle(
+                          color: Colors.white,
+                        ),),
+                  color: Colors.blue,
+                  onPressed: () async {}),
             ),
+            SizedBox(height: 15.0),
+            Container(
+                child: Row(
+              children: <Widget>[
+                Text('Already have an account?'),
+                FlatButton(
+                  textColor: Colors.blue,
+                  child: Text(
+                    'Log in',
+                    style: TextStyle(fontSize: 15),
+                  ),
+                  onPressed: () {
+                    //signup screen
+                    widget.toggleView();
+                  },
+                )
+              ],
+              mainAxisAlignment: MainAxisAlignment.center,
+            ))
           ],
         ),
       ),
